a4f438aa8be16d9f3d435f0fdbc47631ef885126
Ammar Sammour <ammar.n.sammour@gmail.com>
2019-07-26 11:51:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function increaseRankBy(n){
   .getElementById('app')
   .querySelectorAll('ul.ranked-list li');
   for(let i=0; i > lis.length; i++){
-  is[i].innerHTML = (i + n).toString();
+  is[i].innerHTML = (i + n).parseInt();
 }
   )
   }
