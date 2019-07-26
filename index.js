52e4b9a81407052537d5bb6e5fa2fea4587c2dc2
Ammar Sammour <ammar.n.sammour@gmail.com>
2019-07-26 11:48:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ function nestedTarget(){
   return document.querySelector( '#nested .target' );
 }
 function increaseRankBy(n){
+  return(
   const rankedLists = document.querySelectorAll( '.ranked-list' )
   const firstList = rankedLists[ 0 ]
   const secondList = rankedLists[ 1 ]
 
   let children = firstList.children
   for ( let i = 0, l = children.length; i < l; i++ ) {
-    ( parseInt( children[ i ].innerHTML ) )
+    ( parseInt( children[ i ].innerHTML ) )}
   }
